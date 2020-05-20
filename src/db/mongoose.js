1f0692e91b27b70e26e80d6a7ea2839134516b7b
RafaelMurad <rflmurad@gmail.com>
2020-05-20 21:49:07 +0100

    creating modules with mongoose

---
+++ src/db/mongoose.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String
+  },
+
+  age: {
+    type: Number
+  }
+});
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String
+  },
+
+  completed: {
+    type: Boolean
+  }
+});
+
+const task = new Task({
+  description: "clean the house",
+  completed: true
+});
+
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
+
+// const me = new User({
+//   name: "Rafael",
+//   age: 27
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error", error);
+//   });
