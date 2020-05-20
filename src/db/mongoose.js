1d91fa585d32a95b7b656b9deb43aae8e86c27bb
RafaelMurad <rflmurad@gmail.com>
2020-05-20 23:28:24 +0100

    integrating validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -6,29 +7,77 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useCreateIndex: true
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String
-  },
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   },
 
-  age: {
-    type: Number
-  }
-});
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Use a valid email");
+//       }
+//     }
+//   },
+
+//   password: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     minlength: 6,
+//     validate(value) {
+//       if (value.toLowerCase().includes("password")) {
+//         throw new Error("Password must not contain 'password");
+//       }
+//     }
+//   },
+
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error("Age must be a positive number");
+//       }
+//     }
+//   }
+// });
 
 const Task = mongoose.model("Task", {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
 
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 });
 
+// const me = new User({
+//   name: "   Lucas     ",
+//   email: "rfl@gmail.com     ",
+//   password: "221382"
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error", error);
+//   });
+
 const task = new Task({
-  description: "clean the house",
-  completed: true
+  description: "clean the house"
 });
 
 task
@@ -39,16 +88,3 @@ task
   .catch((error) => {
     console.log("error", error);
   });
-
-// const me = new User({
-//   name: "Rafael",
-//   age: 27
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log("Error", error);
-//   });
